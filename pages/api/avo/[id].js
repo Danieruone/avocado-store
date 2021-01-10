bf590ac10e30e0522183aac452b980581f25d2ec
Daniel <danieruone@gmail.com>
2021-01-09 20:37:43 -0500

    first commit

---
+++ pages/api/avo/[id].js
@@ -0,0 +1,15 @@
+import DB from "../../../database/db.js";
+
+const AvoDetail = async (req, res) => {
+  try {
+    const db = new DB();
+    const avoId = req.query.id;
+    const avo = await db.getById(avoId);
+    res.status(200).json(avo);
+  } catch (e) {
+    console.error(e);
+    res.status(404).end();
+  }
+};
+
+export default AvoDetail;
---
+++ pages/api/avo/index.js
@@ -0,0 +1,21 @@
+import DB from "../../../database/db.js";
+
+const allAvos = async (req, res) => {
+  try {
+    const db = new DB();
+    const allEntries = await db.getAll();
+    const lenght = allEntries.length;
+    res.json({
+      lenght,
+      data: allEntries,
+    });
+  } catch (e) {
+    console.error(e);
+    res.statusCode = 500;
+    res.end(
+      JSON.stringify({ length: 0, data: [], error: "Something went wrong" })
+    );
+  }
+};
+
+export default allAvos;
