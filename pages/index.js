b4766e93ca687d7c335f88906e0cca42b7e32ecc
Daniel <danieruone@gmail.com>
2021-01-12 00:02:59 -0500

    title and cards hover

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { AvocadoCard } from "@components/AvocadoCard";
 import styles from "@styles/home.module.css";
+import Avocado from "@components/Avocado";
 
 const axios = require("axios");
 
@@ -16,6 +17,11 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      <div className={styles.title}>
+        <h1>Avo</h1>
+        <Avocado size={70}/>
+        <h1>Store</h1>
+      </div>
       <div className={styles.listContainer}>
         {avocados.map((avocado) => {
           return <AvocadoCard key={avocado.id} {...avocado} />;
