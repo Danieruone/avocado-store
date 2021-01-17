0e645ad312aeb407164a7eaed9eeead31a64aecc
Daniel <danieruone@gmail.com>
2021-01-16 21:35:32 -0500

    detail page description

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useRouter } from "next/router";
+import styles from "@styles/detail.module.css";
 
 const DetailPage = () => {
   const { query } = useRouter();
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState(null);
 
   useEffect(() => {
     if (query.id) {
@@ -11,15 +12,39 @@ const DetailPage = () => {
         .fetch(`/api/avo/${query.id}`)
         .then((response) => response.json())
         .then((data) => {
+          console.log(data);
           setProduct(data);
         });
     }
   }, [query.id]);
 
   return (
-    <div>
-      <p>{product.name}</p>
-    </div>
+    <Fragment>
+      {product == null ? (
+        <h1>Product not found</h1>
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.detailContainer}>
+            <div className={styles.cartDetail}>
+              <img src={product.image} />
+              <div>
+                <h1>{product.name}</h1>
+                <p>{product.price}</p>
+                <p>{`SKU: ${product.sku}`}</p>
+                <div>
+                  <input placeholder="Quantity"></input>
+                  <input type="button" value="Add to Cart"></input>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h1>About this avocado</h1>
+              <p>{product ? 'description' : product.attributes.description}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
